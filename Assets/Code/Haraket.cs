4d9ff5856bfbf3b1b393aa392fbb20663a0738b5
IcedFire34 <omar.faruk.bozkaya@gmail.com>
2023-08-07 22:07:50 +0300

    ilk commit

---
+++ Assets/Code/Haraket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Haraket : MonoBehaviour
+{
+    public int hız = 1;
+    public int ZıplamaGücü = 2;
+    Rigidbody2D Rigid;
+    public float yatay;
+    public float dikey;
+    void Start()
+    {
+        Rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+      YatayHaraket();
+      Zıpla();
+    }
+    void YatayHaraket()
+    {
+      yatay = Input.GetAxis("Horizontal");
+      Rigid.AddForce(new Vector3(yatay * hız,0,0));      
+    }
+    void Zıpla()
+    {
+        dikey = Input.GetAxis("Vertical");
+        Rigid.AddForce(new Vector3(0, dikey * ZıplamaGücü, 0));
+    }
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    int puan;
+    public Text PuanText;    
+    public int hız = 7;
+    public int ZıplamaGücü = 150;
+    Rigidbody2D Rigid;
+    public float yatay;
+    public float dikey;
+    
+    void Start()
+    {
+        Rigid = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        YatayHaraket();
+        PuanText.text = puan.ToString();
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Coin")
+            {
+                puan++;
+                Destroy(collision.gameObject);
+            }
+            if (collision.gameObject.tag == "Yer")
+            {
+                Zıpla();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            puan++;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Yer")
+        {
+            Zıpla();
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Yer")
+        {
+            Zıpla();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Yer")
+        {
+            Zıpla();
+        }
+    }
+    void Zıpla()
+    {
+        dikey = Input.GetAxis("Vertical");
+        Rigid.AddForce(new Vector3(0, dikey * ZıplamaGücü, 0));
+    }
+    void YatayHaraket()
+    {
+        yatay = Input.GetAxis("Horizontal");
+        Rigid.AddForce(new Vector3(yatay * hız, 0, 0));
+    }
+}
